16b0e93633d9843dd696ee9cd6b2fc4bc2a55a44
HgAn <dcgame205@gmail.com>
2025-03-10 23:43:00 +0700

    update

--- magnews2-master/js/category.js
+++ magnews2-master/js/category.js
@@ -3,21 +3,24 @@ dayjs.extend(dayjs.extend(window.dayjs_plugin_relativeTime));
 const elmArticlesOther = document.getElementById('articles-other');
 const categoryName = document.getElementById('category-name');
 const elmMainArticles = document.getElementById('main-article-category');
+const btnLoadmore = document.getElementById('funLoadMore');
+const elMyPagination = document.getElementById('total-page');
 
+//  CHANGE CATEGORY
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = parseInt(urlParams.get('id'));
+let currentPage = parseInt(urlParams.get('page'));
+if (isNaN(currentPage)) currentPage = 1;
 
+
+// RENDER MAIN ARTICLE
 API.get(`categories_news/${id}/articles?limit=14&page=1`).then(res => {
     let data = res.data.data;
-    console.log(data);
 
-    // RENDER MAIN ARTICLE BY CATEGORY
     let htmlLarge = '';
     let htmlExtra = '';
     data.forEach((item, index) => {
-
-    
         let relativeTime = dayjs(item.publish_date).fromNow();
         htmlLarge = /* html */ `				
         <div class="col-md-6 p-rl-1 p-b-2">
@@ -82,40 +85,104 @@ API.get(`categories_news/${id}/articles?limit=14&page=1`).then(res => {
         </div>
     </div>`;
     elmMainArticles.innerHTML = html;
-
-    // RENDER ARTICLES OTHER
-    let htmlOther = /* html */ ``;
-    data.forEach((item, index) => {
-        if (index >= 4 && index < 14) {
-            let relativeTime = dayjs(item.publish_date).fromNow();
-            categoryName.innerText = item.category.name;
-            htmlOther += /* html */`
-            <div class="col-sm-6 p-r-25 p-r-15-sr991">
-                <!-- Item latest -->
-                <div class="m-b-45">
-                    <a href="#" class="wrap-pic-w hov1 trans-03">
-                        <img src="${item.thumb}" alt="IMG">
-                    </a>
-    
-                    <div class="p-t-16">
-                        <h5 class="p-b-5">
-                            <a href="blog-detail-01.html" class="f1-m-3 cl2 hov-cl10 trans-03">
-                                ${item.title}
-                            </a>
-                        </h5>
-    
-                        <span class="cl8">
-                            <a href="#" class="f1-s-4 cl8 hov-cl10 trans-03">by ${item.author}</a>
-    
-                            <span class="f1-s-3 m-rl-3"> - </span>
-    
-                            <span class="f1-s-3">${relativeTime}</span>
-                        </span>
-                    </div>
-                </div>
-            </div>`;
-        }
-
-    });
-    elmArticlesOther.innerHTML = htmlOther;
 });
+
+getArticles(currentPage);
+// btnLoadmore.addEventListener('click', function() {
+//     currentPage++;
+//     btnLoadmore.innerText = 'đang tải...';
+//     btnLoadmore.disabled = true;
+//     getArticles(currentPage);
+// })
+
+// btn ACTION
+elMyPagination.addEventListener('click', function(e) {
+    let elm = e.target;
+    if (elm.classList.contains('page-item')) {
+        currentPage = parseInt(elm.innerText); 
+    }else if (elm.classList.contains('prevous')) {
+        currentPage-- ;
+    }else if (elm.classList.contains('next')) {
+        currentPage++ ;
+    }
+    
+    
+    
+    addOrUpdateUrlParameter('page', currentPage);
+    getArticles(currentPage);
+});
+
+// URL
+function addOrUpdateUrlParameter(key, value) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    urlParams.set(key, value);
+    const newURL = window.location.pathname + '?' + urlParams.toString();
+    history.pushState(null, '', newURL)
+}
+
+// RENDER ARTICLES OTHER
+function getArticles(page = 1) {
+    API.get(`categories_news/${id}/articles?limit=14&page=${page}`).then(res => {
+        let data = res.data.data;
+        let total = res.data.meta.last_page;
+        renderPagination(total);
+
+        let htmlOther = /* html */ ``;
+        data.forEach((item, index) => {
+            if (index >= 4 && index < 14) {
+                let relativeTime = dayjs(item.publish_date).fromNow();
+                categoryName.innerText = item.category.name;
+                htmlOther += /* html */`
+                <div class="col-sm-6 p-r-25 p-r-15-sr991">
+                    <!-- Item latest -->
+                    <div class="m-b-45">
+                        <a href="#" class="wrap-pic-w hov1 trans-03">
+                            <img src="${item.thumb}" alt="IMG">
+                        </a>
+        
+                        <div class="p-t-16">
+                            <h5 class="p-b-5">
+                                <a href="blog-detail-01.html" class="f1-m-3 cl2 hov-cl10 trans-03">
+                                    ${item.title}
+                                </a>
+                            </h5>
+        
+                            <span class="cl8">
+                                <a href="#" class="f1-s-4 cl8 hov-cl10 trans-03">by ${item.author}</a>
+        
+                                <span class="f1-s-3 m-rl-3"> - </span>
+        
+                                <span class="f1-s-3">${relativeTime}</span>
+                            </span>
+                        </div>
+                    </div>
+                </div>`;
+            }
+
+        });
+        elmArticlesOther.innerHTML = htmlOther;
+        // elmArticlesOther.innerHTML += htmlOther;
+        // btnLoadmore.innerText = 'xem thêm';
+        // btnLoadmore.disabled = false;
+
+    })  .catch(function (error) {
+        window.location.href = 'index.html';
+      });
+    
+};
+
+// PAGINATION
+function renderPagination(total) {
+    let disabledPrev = currentPage === 1 ? 'pointer-event-none' : '';
+    let disabledNext = currentPage === total ? 'pointer-event-none' : '';
+
+    let html = `<a href="#" class="btn btn-outline-success prevous ${disabledPrev}">Prevous</a>`;
+    for (let i = 1; i <= total; i++) {
+        const active = i === currentPage ? 'pagi-active pointer-event-none' : '';
+        html += /* html */ `<a href="#" class="flex-c-c pagi-item hov-btn1 trans-03 m-all-7 page-item ${active}">${i}</a>`;
+    }
+    html += `<a href="#" class="btn btn-outline-success next ${disabledNext}">Next</a>`;
+    elMyPagination.innerHTML = html;
+}
+
